724da47a117c72ec22f524f0d94a38700378f17b
Antonio <tonioufa@gmail.com>
2022-10-04 16:58:24 +0100

    loads proposal info from utils file

--- index.ts
+++ index.ts
@@ -9,6 +9,9 @@ import { Interface } from '@ethersproject/abi'
 import {
   GOVERNOR_BRAVO_IMPLEMENTATION_ABI,
   ENS_PUBLIC_RESOLVER_ABI,
+  PROPOSAL_BODY,
+  ENS_RECORD_KEY,
+  ENS_RECORD_VALUE,
 } from './utils'
 
 const GOVERNOR_BRAVO_ADDRESS: string =
@@ -31,9 +34,9 @@ const setTextCalldata = ensPublicResolverInterface.encodeFunctionData(
     // node: The node to update
     namehash('v3-core-license-grants.uniswap.eth'),
     // key: The key to set: '[your-projects-additional-use-grant-title]',
-    'Matter Labs Uni v3 Additional Use Grant',
+    ENS_RECORD_KEY,
     // value: The text data value to set: '[your-additional-use-grant-description]',
-    `Matter Labs is granted an additional use grant to use the Uniswap V3 Core software code (which is made available to Matter Labs subject to license available at https://github.com/Uniswap/v3-core/blob/main/LICENSE (the “Uniswap Code”)). As part of this additional use grant, Matter Labs receives license to use the Uniswap Code for the purposes of a full deployment of the Uniswap Protocol v3 onto the zkSync blockchain. Matter Labs is permitted to use subcontractors to do this work. This license is conditional on Matter Labs complying with the terms of the Business Source License 1.1, made available at https://github.com/Uniswap/v3-core/blob/main/LICENSE.)`,
+    ENS_RECORD_VALUE,
   ]
 )
 
@@ -62,68 +65,7 @@ const signatures = ['']
 const calldatas = [setTextCalldata]
 
 // the description of the proposal.
-const description = `# Deploy Uniswap V3 on zkSync ## FranklinDAO (Prev. Penn Blockchain) is creating this proposal in partnership with Matter Labs to Deploy Uniswap V3 on zkSync.
-
-Matter Labs: @zkSync
-
-**Proposal History**
-
-The [consensus check](https://snapshot.org/#/uniswap/proposal/0xe6ad2033b04559b3db6be7fa9993f05ee68f43f1061c632382c88e367e76bfb1) passed with 24M (~100%) YES votes. The [temperature check](https://snapshot.org/#/uniswap/proposal/0xab1dbb6e3486073b81cc64b81d15d24a7bf82efcfc94e533de0779250df06ba9) passed with 15M (~100%) YES votes.
-
-**Summary**
-
-To support Uniswap's multichain mission and expand cross-chain experiences, we propose the deployment of Uniswap V3 to zkSync 2.0 on behalf of the community.
-
--   zkSync ecosystem has over [100 projects](https://ecosystem.zksync.io/) committed to launching on mainnet, including top DeFi protocols, infrastructure, on/off ramps, etc.
-
--   Deploying on zkSync will onboard new users & increase user activity on Uniswap by decreasing costs compared to Ethereum without security degradation
-
--   zkSync shares Ethereum's ethos as a free open-source project with a commitment to personal sovereignty, decentralization and community ownership
-
-We welcome feedback from the community on the proposal, including suggestions on how it can be improved.
-
-**About zkSync**
-
-zkSync 2.0 is a [ZK rollup](https://ethereum.org/en/developers/docs/scaling/zk-rollups/) that supports generalized EVM compatibility for the Ethereum blockchain. The primary benefit of zkSync 2.0 is that developers who have created EVM dApps can port to zkSync 2.0 effortlessly and realize significantly lower gas fees and more transactions per second without compromising on security.
-
-zkSync 2.0 is a significant leap forward in Layer 2 technologies with long awaited improvements and benefits for Ethereum developers:
-
--   EVM Compatible - supporting generalized EVM smart contracts on a ZK rollup making it easy to deploy existing dApps 
-
--   ToolChain Compatible - able to port smart contracts with existing tools
-
--   Ethos Compatible - aligned with the ethos of decentralization and open-source
-
--   Certainty - using zero knowledge proofs offering certainty of security not probability 
-
--   Future Proof - ecosystem partners that adopt zkSync 2.0 now will enjoy all future improvements without the need to change their code
-
-There is broad consensus that ZK rollups are the endgame for scaling Ethereum. zkSync's EVM compatibility, ease of use, and composability will accelerate developer and retail adoption. Top researchers including [Vitalik Buterin](https://youtu.be/XW0QZmtbjvs) recognize ZK rollups as the long term scaling solution.
-
-**Security & Bridges**
-
-ZK rollups are the most secure scalability solution available today as they rely purely on math to fully inherit the security of Ethereum. There is a general L1<>L2 communication bridge which will support arbitrary message passing and secured by validity proof and Ethereum consensus.
-
-Bridge validators can't pass an incorrect message or change the content, the worst case would be to censor everyone. Importantly, we'll be building out additional safety functionality and monitoring off & on-chain activity.
-
-[Security is top of mind](https://docs.zksync.io/dev/security/approach/#_1-security-by-correctness) for zkSync. We are currently working with tier-1 auditors for zkSync 2.0 and specifically in the review process for the bridge code. Audits will be conducted before each major upgrade. Besides audits, we offer a substantial [bug bounty program](https://docs.zksync.io/dev/security/bug-bounty/).
-
-**Proposal**
-
-There's significant value in Uniswap being available on an EVM compatible ZK rollup. Deploying early on zkSync helps solidify Uniswap's place as the number one DEX and a thought leader.
-
-Importantly, it will help grow a large list of projects that can be built on Uniswap V3. Established projects like Argent, Curve, and Yearn have committed to launch along with [over 100 more projects](https://ecosystem.zksync.io/) and big infrastructure players like Chainlink, The Graph, Gnosis are supporting the ecosystem. Growing the public smart contract libraries interfacing and using Uniswap v3 codebase will solidify Uniswap's influence in the Ethereum ecosystem which is moving on to ZK rollups.
-
-While the zkSync ecosystem is already experiencing very fast growth, the team is planning  programs to attract and fund innovative projects and research partners to accelerate the network's adoption and in turn, Uniswap's usage.
-
-**License Exemption**
-
-We are requesting an exemption via an Additional Use Grant (license change enacted via the ENS domain uniswap.eth) that would allow Matter Labs to use the Licensed Work to deploy it on zkSync provided that the deployment is subject to Ethereum layer 1 Uniswap Protocol governance and control. Uniswap V3 will be deployed on zkSync by Matter Labs through the "[Deploy Uniswap V3 Script](https://github.com/Uniswap/deploy-v3#deploy-uniswap-v3-script)" albeit we may need to modify the compilation step with approval from the Uniswap Labs team.
-
-**Timeline**
-
-Following the Governance Proposal we will be ready to move forward with the Uniswap V3 deployment on zkSync.\
-zkSync has been on testnet since February 2022 and plans to launch mainnet [early October](https://blog.matter-labs.io/100-days-to-mainnet-6f230893bd73). A timely assessment of the deployment of Uniswap v3 code to zkSync is important: while deploying on zkSync is fast and easy because it's fully EVM compatible, we estimate the full effort will take 4-6 weeks given Uniswap's relevance. This allows for proper testing, communication to the community and engagement with the broader zkSync ecosystem.`
+const description = PROPOSAL_BODY
 
 async function main() {
   try {
